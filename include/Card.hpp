5b430940241156dabcbc8286738e91fa48b54d76
Colin <sadowitzc@gmail.com>
2026-08-14 17:41:30 -0400

    Updated Card class with == overload

--- include/Card.hpp
+++ include/Card.hpp
@@ -1,8 +1,11 @@
 #ifndef CARD_HPP
 #define CARD_HPP
 
+#include <magic_enum/magic_enum.hpp>
+#include <spdlog/spdlog.h>
+
 namespace game {
-  enum CardType {
+  enum class CardType {
     RED,
     GREEN,
     BLUE,
@@ -10,7 +13,7 @@ namespace game {
     WILD
   };
 
-  enum CardValue {
+  enum class CardValue {
     ZERO,
     ONE,
     TWO,
@@ -25,14 +28,59 @@ namespace game {
     PLUS_TWO,
     REVERSE,
     CHANGE_COLOR,
-    CHANGE_COLOR_RED,
-    CHANGE_COLOR_GREEN,
-    CHANGE_COLOR_BLUE,
-    CHANGE_COLOR_YELLOW,
     CHANGE_COLOR_PLUS_FOUR
   };
 
-  struct Card {
+  class Card {
+    public:
+      Card(CardType type, CardValue value) {
+        this->type = type;
+        this->value = value;
+      }
+
+      void ChangeColor(CardType type) {
+        if (this->type == CardType::WILD) {
+          spdlog::info("[Card] [ChangeColor] {} to {}", magic_enum::enum_name(this->type), magic_enum::enum_name(type));
+          switch (type) {
+            case(CardType::RED):
+              this->type = CardType::RED;
+              break;
+            case(CardType::GREEN):
+              this->type = CardType::GREEN;
+              break;
+            case(CardType::BLUE):
+              this->type = CardType::BLUE;
+              break;
+            case(CardType::YELLOW):
+              this->type = CardType::YELLOW;
+              break;
+            case(CardType::WILD):
+              break;
+          }
+        }
+      }
+
+      void Print() {
+        spdlog::info("[Card] [Print] {} {}", magic_enum::enum_name(this->type), magic_enum::enum_name(this->value));
+      }
+
+      void Reset() {
+        // Is this card actually a wild?
+        if (this->value == CardValue::CHANGE_COLOR || this->value == CardValue::CHANGE_COLOR_PLUS_FOUR) {
+          spdlog::info("[Card] [Reset] {} to {}", magic_enum::enum_name(this->type), magic_enum::enum_name(CardType::WILD));
+          this->type = CardType::WILD;
+        }
+      }
+
+      CardType GetType() const { return this->type; }
+
+      CardValue GetValue() const { return this->value; }
+
+      bool operator==(const Card& other) {
+        return (this->type == other.GetType() && this->value == other.GetValue());
+      }
+
+    private:
     CardType type;
     CardValue value;
   };
