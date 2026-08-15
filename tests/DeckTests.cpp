2535385ea080a3a9d9233c49e04cb09ce78481af
Colin <sadowitzc@gmail.com>
2026-08-15 15:10:48 -0400

    Removed non functional tests

--- tests/DeckTests.cpp
+++ tests/DeckTests.cpp
@@ -1,6 +1,45 @@
 #include "Deck.hpp"
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE() {
+TEST_CASE("[DrawCards]") {
+  std::vector<game::Card> cards = std::vector<game::Card>();
+  for (int i = 0; i < 4; i++) {
+      game::Card card0{static_cast<game::CardType>(i), game::CardValue::ZERO};
+      cards.emplace_back(card0);
+      for (int j = 0; j < 2; j++) {
+        for(int k = 1; k < 13; k++) {
+          game::Card card1{static_cast<game::CardType>(i), static_cast<game::CardValue>(k)};
+          cards.emplace_back(card1);
+        }
+      }
+  }
 
+  for (auto card : cards) {
+    spdlog::info("[DeckTests] {}", card.Print());
+  }
+
+  game::Deck deck = game::Deck(cards);
+
+  SECTION("No Cards") {
+    auto drawnCards = deck.DrawCards(0);
+    REQUIRE(!drawnCards);
+  }
+
+  SECTION("Single Card") {
+    auto drawnCards = deck.DrawCards(1);
+    REQUIRE(drawnCards);
+    REQUIRE(drawnCards.value().size() == 1);
+  }
+
+  SECTION("Two Cards") {
+
+  }
+
+  SECTION("Four Cards") {
+
+  }
+
+  SECTION("Until Playable") {
+
+  }
 }
